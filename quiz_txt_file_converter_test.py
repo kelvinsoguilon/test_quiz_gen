819f6003d852aaa9eda8224cf3777a22bdddc2b7
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-10 10:14:33 +0800

    Added pseudocode to help code the program more efficiently

---
+++ quiz_txt_file_converter_test.py
@@ -0,0 +1,7 @@
+#define quiz generator program
+#create a file path or directory for the txt file
+#loop the program until user wants to exit
+#ask the user for the quiz questions
+#ask the choices
+#ask the answer
+#save all the information to the txt file
