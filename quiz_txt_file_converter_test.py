0aa85f1192553e895ad5abb2d53e740bbcd22d36
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-10 10:35:35 +0800

    Fixed comment indentations and added some comments to other variable

--- quiz_txt_file_converter_test.py
+++ quiz_txt_file_converter_test.py
@@ -2,16 +2,16 @@
 def quiz_generator():
     #create a file path or directory for the txt file
     filename = "quiz.txt"
-    count = 1
-#loop the program until user wants to exit
+    count = 1 #added count variable to track number of questions
+    #loop the program until user wants to exit
     with open(filename, "a") as file:
         while True:
+            #ask the user for the quiz questions
             questions = input(f"Enter your quiz question no.{count}. (Press e to exit the program): ")
-            if questions.lower == 'e':
+            if questions.lower == 'e': #condition to exit program
                 print("Exiting the program.")
                 break
             count += 1
-#ask the user for the quiz questions
-#ask the choices
-#ask the answer
-#save all the information to the txt file
+        #ask the choices
+        #ask the answer
+        #save all the information to the txt file
