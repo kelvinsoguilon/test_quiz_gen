cb6a317ddcc456e34ed9b80940bfd5189fe8be8a
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-10 10:32:32 +0800

    Asks the user to input questions as well as condition to exit the program. Added count value to track the number of questions being added to the txt file.

--- quiz_txt_file_converter_test.py
+++ quiz_txt_file_converter_test.py
@@ -1,8 +1,16 @@
 #define quiz generator program
 def quiz_generator():
     #create a file path or directory for the txt file
-    filename = ""
+    filename = "quiz.txt"
+    count = 1
 #loop the program until user wants to exit
+    with open(filename, "a") as file:
+        while True:
+            questions = input(f"Enter your quiz question no.{count}. (Press e to exit the program): ")
+            if questions.lower == 'e':
+                print("Exiting the program.")
+                break
+            count += 1
 #ask the user for the quiz questions
 #ask the choices
 #ask the answer
